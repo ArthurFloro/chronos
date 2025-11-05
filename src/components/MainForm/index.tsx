838540a3e87dcad3d08b0cf2fa411dac7f539710
ArthurFloro <arthur.floro2006@gmail.com>
2025-11-05 18:34:30 -0300

    Context API - useContext + useState (estado + contexto)

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,10 +2,26 @@ import { PlayCircleIcon } from "lucide-react";
 import { Button } from "../Button";
 import { Cycles } from "../Cycles";
 import { Input } from "../Input";
+import { useTaskContext } from "../../contexts/TaskContext";
 
 export function MainForm() {
+  const { setState } = useTaskContext();
+
+  function handleClick() {
+    setState((prevState) => {
+      return {
+        ...prevState,
+        formattedSecondsRemaining: "21:00",
+      };
+    });
+  }
+
   return (
     <form className="form" action="">
+      <button type="button" onClick={handleClick}>
+        Clicar
+      </button>
+
       <div className="formRow">
         <Input
           labelText="Tarefa"
