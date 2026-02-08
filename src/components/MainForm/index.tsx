24a19a33bce7f4ba92cd3a608a8213d1e6f1182c
ArthurFloro <arthur.floro2006@gmail.com>
2026-02-08 12:56:00 -0300

    trabalhando com o estado antes e depois de setState (getNextCycle)

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -5,10 +5,13 @@ import { Input } from "../Input";
 import { useRef } from "react";
 import type { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
 
 export function MainForm() {
-  const { setState } = useTaskContext();
+  const { state, setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
+
+  const nextCycle = getNextCycle(state.currentCycle);
 
   function handleNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -40,9 +43,9 @@ export function MainForm() {
         ...prevState,
         config: { ...prevState.config },
         activeTask: newTask,
-        currentCycle: 1,
+        currentCycle: nextCycle,
         secondsRemaining,
-        formattedSecondsRemaining: `${Math.floor(secondsRemaining / 60)}`,
+        formattedSecondsRemaining: "00:00",
         tasks: [...prevState.tasks, newTask],
       };
     });
